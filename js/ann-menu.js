5b8f7bf96dec0183731b62d917b65fe9fe985b1d
Epureanu Florin Claudiu <lamaieverde@gmail.com>
2016-02-20 13:13:53 +0100

    varie modifiche messo lable clicabile nel menu filtri e grupi

--- js/ann-menu.js
+++ js/ann-menu.js
@@ -1,25 +1,13 @@
 function ToogleSlideCitazione(){
-	var elemen = document.getElementById("gn-icon-open-main-cites");
+	var elemen = document.getElementById("label-gn-icon-open-main-cites");
 	classie.toggle(elemen, 'gn-icon-opendown');
 	classie.toggle(elemen, 'gn-icon-openup');
 	$("#cites-checkbox").slideToggle('fast');
 }
 function ToogleSlideRetorica(){
-  var elemen = document.getElementById("gn-icon-open-main-retoric");
+  var elemen = document.getElementById("label-gn-icon-open-main-retoric");
   var elemen2 = document.getElementById("retoric-checkbox");
   classie.toggle(elemen, 'gn-icon-opendown');
   classie.toggle(elemen, 'gn-icon-openup');
   $("#retoric-checkbox").slideToggle('fast');
 }
-// function ToggleFilter(){
-//   $("#filter-menu").toggle( "slide" );
-// }
-  // $(document).mousedown(function (e){
-  //   var container = $('#filter-menu');
-  //   var btnFilter = $('#filter');
-  //   if (!container.is(e.target) && container.has(e.target).length === 0 && !btnFilter.is(e.target) && btnFilter.has(e.target).length === 0)
-  //     if(!$('#filter').is(':hidden'))
-  //       if(!$('#filter-menu').is(':hidden')){
-  //         $("#filter-menu").toggle( "slide" );
-  //       }
-  //  });
